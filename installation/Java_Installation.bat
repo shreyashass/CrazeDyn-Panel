c4c33f4152b61c3fa267fdc70d9286a62e90f0a1
shreyashass <zerobrinexgamer@gmail.com>
2025-09-06 15:49:33 +0530

    Delete installation directory

--- installation/Java_Installation.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start https://download.oracle.com/java/24/latest/jdk-24_windows-x64_bin.msi
-exit
--- installation/Python_Installation.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start https://www.python.org/ftp/python/3.13.7/python-3.13.7-amd64.exe
-exit
